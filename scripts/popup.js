60bd83841bdcb8e42b939360b3119ffa82f94439
Dhruvil <shahd883@newschool.edu>
2024-04-21 23:20:26 -0400

    Goodnight for today

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,29 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const searchInput = document.getElementById('searchInput');
 	const resultsList = document.getElementById('resultsList');
+	const folderNamesContainer = document.getElementById('folderNamesContainer');
 
-	// Function to search bookmarks
+	// Search bookmarks
 	function searchBookmarks(query) {
-		chrome.bookmarks.search(query, function (bookmarks) {
-			resultsList.innerHTML = '';
-			bookmarks.forEach(function (bookmark) {
-				const listItem = document.createElement('li');
-				listItem.textContent = bookmark.title;
-				listItem.addEventListener('click', function () {
-					chrome.tabs.create({ url: bookmark.url });
-				});
-				resultsList.appendChild(listItem);
+			chrome.bookmarks.search(query, function (bookmarks) {
+					resultsList.innerHTML = '';
+					folderNamesContainer.innerHTML = '';
+
+					bookmarks.forEach(function (bookmark) {
+							// Fetch the parent folder name
+							const folderNamePromise = new Promise((resolve, reject) => {
+									chrome.bookmarks.get(bookmark.parentId, function (folder) {
+											resolve(folder[0].title);
+									});
+							});
+
+							// Once the folder name is retrieved
+							folderNamePromise.then(folderName => {
+									const folderNameItem = document.createElement('div');
+									folderNameItem.textContent = folderName;
+									folderNamesContainer.appendChild(folderNameItem);
+							});
+
+							// Create and append list item for bookmark suggestion
+							const listItem = document.createElement('li');
+							listItem.textContent = bookmark.title;
+							listItem.addEventListener('click', function () {
+									chrome.tabs.create({ url: bookmark.url });
+							});
+							resultsList.appendChild(listItem);
+					});
 			});
-		});
 	}
 
-	// Event listener for input
+	// Input
 	searchInput.addEventListener('input', function () {
-		const query = searchInput.value.trim();
-		if (query.length > 0) {
-			searchBookmarks(query);
-		} else {
-			resultsList.innerHTML = '';
-		}
+			const query = searchInput.value.trim();
+			if (query.length > 0) {
+					searchBookmarks(query);
+			} else {
+					resultsList.innerHTML = '';
+					folderNamesContainer.innerHTML = '';
+			}
 	});
 });
